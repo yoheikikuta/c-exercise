75fbdf9bc8ef592732608d97085b1d810e1abcd6
Yohei Kikuta <diracdiego@gmail.com>
2018-10-02 22:57:57 +0900

    Done basic-4

---
+++ Basic-Declarations-and-Expressions/4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char c1 = 'X';
+    char c2 = 'M';
+    char c3 = 'L';
+
+    printf("The reverse of %c%c%c is %c%c%c\n", c1, c2, c3, c3, c2, c1);
+
+    return 0;
+}
