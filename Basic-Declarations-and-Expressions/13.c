728de7a07fa9060d86eaac8f799a14e05aaf4f33
Yohei Kikuta <diracdiego@gmail.com>
2018-10-03 23:43:08 +0900

    Modify basic-13

--- Basic-Declarations-and-Expressions/13.c
+++ Basic-Declarations-and-Expressions/13.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int v1=0, v2=0, v3=0;
-    int v_1st=0, v_2nd=0, v_3rd=0;
+    int v_max_12=0, v_max_123=0;
 
     printf("Please input 1st int: ");
     scanf("%i", &v1);
@@ -11,39 +12,10 @@ int main() {
     printf("Please input 3rd int: ");
     scanf("%i", &v3);
 
-    if (v1 >= v2) {
-        if (v3 > v1) {
-            v_1st = v3;
-            v_2nd = v1;
-            v_3rd = v2;
-        } else if (v2 > v3) {
-            v_1st = v1;
-            v_2nd = v2;
-            v_3rd = v3;
-        } else {
-            v_1st = v1;
-            v_2nd = v3;
-            v_3rd = v2;
-        }
-    } else {
-        if (v3 > v2) {
-            v_1st = v3;
-            v_2nd = v2;
-            v_3rd = v1;
-        } else if (v1 > v3) {
-            v_1st = v2;
-            v_2nd = v1;
-            v_3rd = v3;
-        } else {
-            v_1st = v2;
-            v_2nd = v3;
-            v_3rd = v1;
-        }
-    }
+    v_max_12 = (v1 + v2 + abs(v1 - v2)) / 2;
+    v_max_123 = (v_max_12 + v3 + abs(v_max_12 - v3)) / 2;
 
-    printf("1st: %i\n" , v_1st);
-    printf("2nd: %i\n" , v_2nd);
-    printf("3rd: %i\n" , v_3rd);
+    printf("The maximum value: %i\n" , v_max_123);
 
     return 0;
 }
