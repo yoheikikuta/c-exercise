f8a760c58799b16767bf7665007c1424168df478
Yohei Kikuta <diracdiego@gmail.com>
2018-10-04 00:19:50 +0900

    Done basic-17

---
+++ Basic-Declarations-and-Expressions/17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int input_sec=0;
+    int hours=0, mins=0, secs=0;
+
+    printf("Input seconds: ");
+    scanf("%i", &input_sec);
+
+    hours = input_sec / (60 * 60);
+    input_sec -= hours * (60 * 60);
+    mins = input_sec / 60;
+    input_sec -= mins * 60;
+    secs = input_sec;
+
+    printf("H:M:S - %i:%i:%i\n", hours, mins, secs);
+
+    return 0;
+}
