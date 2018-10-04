6708b42c9678d76f83517b1a2690638abd4684b9
Yohei Kikuta <diracdiego@gmail.com>
2018-10-05 01:08:48 +0900

    Change judgement conditions

--- Basic-Declarations-and-Expressions/23.c
+++ Basic-Declarations-and-Expressions/23.c
@@ -10,38 +10,8 @@ int main() {
         scanf("%f", &input[i]);
     }
 
-    if (input[0] > input[1]) {
-        if (input[2] > input[0]) {
-           v_max = input[2];
-           v_mid = input[0];
-           v_min = input[1];
-        } else if (input[1] > input[2]) {
-           v_max = input[0];
-           v_mid = input[1];
-           v_min = input[2];
-        } else {
-           v_max = input[0];
-           v_mid = input[2];
-           v_min = input[1];
-        }
-    } else {
-        if (input[2] > input[1]) {
-           v_max = input[2];
-           v_mid = input[1];
-           v_min = input[0];
-        } else if (input[0] > input[2]) {
-           v_max = input[1];
-           v_mid = input[0];
-           v_min = input[2];
-        } else {
-           v_max = input[1];
-           v_mid = input[2];
-           v_min = input[0];
-        }
-    }
-
-    if (v_max < v_mid + v_min) {
-        perimeter = v_max + v_mid + v_min;
+    if ( input[0] < (input[1]+input[2]) || input[1] < (input[0]+input[2]) || input[2] < (input[1]+input[0]) ) {
+        perimeter = input[0] + input[1] + input[2];
         printf("Valid input values and the perimeter is %f.\n", perimeter);
     } else {
         printf("Invalid input values.\n");
