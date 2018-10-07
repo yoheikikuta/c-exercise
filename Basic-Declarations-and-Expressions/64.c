204f2ca6f65e48bdc1b49dccf3c75a134a473ff6
Yohei Kikuta <diracdiego@gmail.com>
2018-10-08 03:23:57 +0900

    Change statistics to be computed

--- Basic-Declarations-and-Expressions/64.c
+++ Basic-Declarations-and-Expressions/64.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main() {
     int input = 1;
-    int sum = 0;
+    int sum = 0, num = 0;
+    float avg = 0;
+    int  max = INT_MIN, min = INT_MAX;
 
     printf("Input integers. Until you input non-positive integer, the integers will be summed.\n");
     while ( input > 0 ) {
         printf("Input integer: ");
         scanf("%i", &input);
+
         if ( input > 0 ) {
             sum += input;
+            num++;
+
+            if ( input >= max ) {
+                max = input;
+            }
+            if ( input < min ) {
+                min = input;
+            }
         }
+
     }
 
-    printf("Sum of the positive integers: %i\n", sum);
+    avg = sum / num;
+    printf("Average value of the positive integers: %f\n", avg);
+    printf("Maximum value of the positive integers: %i\n", max);
+    printf("Minimum value of the positive integers: %i\n", min);
 
     return 0;
 }
