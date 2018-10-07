d31957a8e066dde64a1b978cba28ffcbd8cc1da9
Yohei Kikuta <diracdiego@gmail.com>
2018-10-08 02:36:26 +0900

    Done basic-60
    Skipped from 41 to 59 because these are easy.

---
+++ Basic-Declarations-and-Expressions/60.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    enum WEEK {Sun, Mon, Tue, Wed, Thu, Fri, Sat};
+
+    printf("Sun = %d. \n", Sun);
+    printf("Mon = %d. \n", Mon);
+    printf("Tue = %d. \n", Tue);
+    printf("Wed = %d. \n", Wed);
+    printf("Thu = %d. \n", Thu);
+    printf("Fri = %d. \n", Fri);
+    printf("Sat = %d. \n", Sat);
+
+    return 0;
+}
