ff5a891cf4b18caecbb58f2591819cd61066ffea
Yohei Kikuta <diracdiego@gmail.com>
2018-10-08 12:59:50 +0900

    Done basic-66

---
+++ Basic-Declarations-and-Expressions/66.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num = 50;
+    float rand_max = RAND_MAX;
+    srand(2321);
+
+    FILE *outputfile;
+    outputfile = fopen("out.txt", "w");
+    if (outputfile == NULL) {
+        printf("cannot open\n");
+        exit(1);
+    }
+
+    fprintf(outputfile, "%i\n", num);
+
+    for (int i = 1; i <= num; i++){
+        float normalized_rand = (rand() / rand_max) - 0.5;
+        fprintf(outputfile, "%f\n", normalized_rand);
+    }
+
+    fclose(outputfile);
+
+    return 0;
+}
