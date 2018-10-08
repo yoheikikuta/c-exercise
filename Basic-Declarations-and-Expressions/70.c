48e906bdeae842e15853c1211a146b7ea67a0656
Yohei Kikuta <diracdiego@gmail.com>
2018-10-08 13:47:44 +0900

    Add more condition

--- Basic-Declarations-and-Expressions/70.c
+++ Basic-Declarations-and-Expressions/70.c
@@ -4,7 +4,9 @@ int main(){
     int comb_coeff = 0;
 
     for (int i = 'A' - 0; i <= 'z' - 0; i++) {
-        printf("[%i-%c] ", i, i);
+        if ( (i < '[' - 0) || (i > '`' - 0) ) {
+            printf("[%i-%c] ", i, i);
+        }
     }
     printf("\n");
 
